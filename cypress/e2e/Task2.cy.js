57e0900db0e2182b8320a2b10ecacb10f823a788
samaraghbar <samaraghbar@hotmail.com>
2024-03-21 02:41:25 +0200

    Task2

--- cypress/e2e/Task2.cy.js
+++ cypress/e2e/Task2.cy.js
@@ -9,12 +9,12 @@ describe("task2", ()=>{
 it('add product test case', () => {
    cy.get('h1').contains("Dashboard").should("be.visible")
    cy.get(".fa-bars").should("be.visible").trigger("mouseover").click() 
-   cy.wait(2000)
-   cy.get('p').contains("Catalog").should("be.visible")
-   cy.get('p').contains("Catalog").click()
+   cy.wait(5000)
+   cy.get('p').contains("Catalog").should("be.visible").click()
    cy.get('.nav-link').should("be.visible")
    //cy.get('.nav-treeview').should("be.visible")
    cy.get('p').contains("Products").click()
+   cy.wait(2000)
    cy.get('.float-left').should("be.visible")
    cy.get("[href='/Admin/Product/Create']").contains("Add new").click()
    cy.get('.card-title').should("be.visible")
@@ -22,7 +22,9 @@ it('add product test case', () => {
    cy.get('#Name').type("Laptop 123")
    cy.screenshot({capture:"runner"})
    cy.get('.k-multiselect-clearable').first().type("Computers")
-   cy.get('.fa-dollar-sign').should("be.visible")
+   cy.wait(2000)
+   cy.get('.col-form-label').contains("Price").should("be.visible")
+   cy.get('#Price')
    cy.get('.btn-primary').contains("Save").click()
 
 
@@ -30,33 +32,31 @@ it('add product test case', () => {
 it('Search for product test case', () => {
    cy.get('h1').contains("Dashboard").should("be.visible")
    cy.get(".fa-bars").should("be.visible").trigger("mouseover").click()
-   cy.wait(2000)
-   cy.get('p').contains("Catalog").should("be.visible")
-   cy.get('p').contains("Catalog").click()
+   cy.wait(7000)
+   cy.get('p').contains("Catalog").should("be.visible").click()
    cy.get('.nav-treeview').should("be.visible")
    cy.get('p').contains("Products").click()
    cy.get('h1').contains("Products").should("be.visible")
    cy.get('#SearchProductName').type("laptop123")
    cy.get('#SearchCategoryId').select("Computers")
    cy.get("#search-products").click();
-   cy.get('td').contains("Laptop123").should("be.visible")
+
   
    
 });
 
 it('Delete product test case', () => {
    cy.get('h1').contains("Dashboard").should("be.visible")
-   cy.get(".fa-bars").should("be.visible").trigger("mouseover").click()
    cy.wait(2000)
-   cy.get('p').contains("Catalog").should("be.visible")
-   cy.get('p').contains("Catalog").click()
+   cy.get(".fa-bars").should("be.visible").trigger("mouseover").click()
+   cy.wait(7000)
+   cy.get('p').contains("Catalog").should("be.visible").click()
    cy.get('.nav-treeview').should("be.visible")
    cy.get('p').contains("Products").click()
    cy.get('h1').contains("Products").should("be.visible")
    cy.get('#SearchProductName').type("laptop123")
    cy.get('#SearchCategoryId').select("Computers")
    cy.get("#search-products").click();
-   cy.get('td').contains("Laptop123").should("be.visible")
    cy.get('.checkboxGroups').check()
    cy.get('#delete-selected').click()
 cy.get('#delete-selected-action-confirmation-title').should("be.visible")
